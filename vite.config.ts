687670746bb82f83c5a699e089812239a8e93cdc
shellingfordly <shellingfordly@qq.com>
2022-07-12 15:53:49 +0800

    chore(github pages): fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pixi-games",
   server: { port: 3090 },
   resolve: {
     alias: [
